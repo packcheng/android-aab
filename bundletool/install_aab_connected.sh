1d061d58b0cebe6ca7c810147435281056578e53
packcheng <packcheng_jo@outlook.com>
2021-08-31 23:44:02 +0800

    refactor: 修改相关脚本

---
+++ bundletool/install_aab_connected.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+AAB_NAME="release.aab"
+APKS_NAME="app.apks"
+BUNDLE_JAR="bundletool.jar"
+KS_STORE="packcheng.keystore"
+KEY_ALIAS="packcheng"
+KS_PASS="123456789"
+KEY_PASS="123456789"
+
+currentDir=$(pwd)
+
+echo "移除之前生成的apks"
+rm -f "$currentDir/$APKS_NAME"
+
+echo "准备生成连接设备的apks"
+java -jar "$currentDir/$BUNDLE_JAR" build-apks \
+  --connected-device \
+  --bundle="$currentDir/$AAB_NAME" \
+  --output="$currentDir/$APKS_NAME" \
+  --ks="$currentDir/$KS_STORE" \
+  --ks-pass=pass:$KS_PASS \
+  --ks-key-alias=$KEY_ALIAS \
+  --key-pass=pass:$KEY_PASS
+
+echo "准备安装apks"
+java -jar "$currentDir/$BUNDLE_JAR" install-apks --apks="$currentDir/$APKS_NAME"
---
+++ bundletool/signe.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+UNSIGNED_AAB_NAME="app_unsigned.aab"    #未签名的aab文件名
+SIGNED_AAB_NAME="app_signed.aab"        #已签名的aab文件名
+KEYSTORE_FILE_NAME="packcheng.keystore" #签名文件名
+KEYSTORE_PWD="123456789"
+KEY_ALIAS="packcheng"
+KEY_PWD="123456789"
+
+currentDir=$(pwd)
+echo "当前目录为：$currentDir"
+
+echo "移除之前已签名的aab文件"
+rm -f "$currentDir/$SIGNED_AAB_NAME"
+
+echo "准备开始签名"
+jarsigner -digestalg SHA1 -sigalg SHA1withRSA \
+  -keystore "$currentDir/$KEYSTORE_FILE_NAME" \
+  -storepass "$KEYSTORE_PWD" \
+  -keypass "$KEY_PWD" \
+  -signedjar "$currentDir/$SIGNED_AAB_NAME" "$currentDir/$UNSIGNED_AAB_NAME" "$KEY_ALIAS"
